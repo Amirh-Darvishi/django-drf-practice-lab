454d5629732c41fb6df9bd6b40cb9ec1f7752418
Amirhossein <amirhhdr13832@gmail.com>
2026-07-20 14:44:32 +0330

    Update models.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -71,12 +71,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         # Simplest possible answer: Yes, always
         return True
 
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
-
 
 
 
