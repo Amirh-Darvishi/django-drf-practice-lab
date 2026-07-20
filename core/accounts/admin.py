ae67f0187c1a2ed618f0db453c73cf69f151d344
Amirhossein <amirhhdr13832@gmail.com>
2026-07-20 15:29:11 +0330

    Update admin.py

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -82,7 +82,7 @@ class UserAdmin(BaseUserAdmin):
         (None, {
             "classes": ("wide",),
             "fields": (
-                "email", "password1", "password2", "is_sttaf",
+                "email", "password1", "password2", "is_staff",
                 "is_active", "groups", "user_permissions"
             )}
         ),
