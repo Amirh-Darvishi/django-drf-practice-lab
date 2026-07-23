8c377e544eab24434b4c30c289a5c13dccf8de49
Amirhossein <amirhhdr13832@gmail.com>
2026-07-23 23:23:34 +0330

    Update urls.py

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from accounts.api.v1.views import * 
+
+
+app_name = 'api-v1'
 
 urlpatterns = [
-    
+
+    # registration
+    path('registration/',RegistrationApiView.as_view(), name='registration' )
 ]
