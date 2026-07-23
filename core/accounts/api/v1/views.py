24f78ed519d47e44cbb2ffbe4b9dc3fe16c55578
Amirhossein <amirhhdr13832@gmail.com>
2026-07-23 23:23:37 +0330

    Update views.py

---
+++ core/accounts/api/v1/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics 
+from accounts.api.v1.serializers import *
+from rest_framework.response import Response
+from rest_framework import status
+
+
+
+
+class RegistrationApiView(generics.GenericAPIView):
+
+    serializer_class = RegistrationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                'email':serializer.validated_data['email']
+            }
+            return Response(data,status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
