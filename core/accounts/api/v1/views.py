c75be3c25e2a473a2f7df3793ce9dd597829f462
Amirhossein <amirhhdr13832@gmail.com>
2026-07-25 18:50:54 +0330

    Update views.py

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -2,7 +2,10 @@ from rest_framework import generics
 from accounts.api.v1.serializers import *
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 
 
@@ -17,6 +20,33 @@ class RegistrationApiView(generics.GenericAPIView):
             data = {
                 'email':serializer.validated_data['email']
             }
-            return Response(data,status.HTTP_201_CREATED)
+            return Response(data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
+
+
+
+class CustomDiscardAuthToken(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
