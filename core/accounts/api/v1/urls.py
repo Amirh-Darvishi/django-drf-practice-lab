1ff4c1b540a46952a0943f3e8dc77b0b5f6e1ae7
Amirhossein <amirhhdr13832@gmail.com>
2026-07-26 01:05:03 +0330

    Update urls.py

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from accounts.api.v1.views import * 
 #from rest_framework.authtoken.views import ObtainAuthToken
-
+from rest_framework_simplejwt.views import (TokenRefreshView, TokenVerifyView)
 
 
 app_name = 'api-v1'
@@ -11,5 +11,13 @@ urlpatterns = [
     # registration
     path('registration/',RegistrationApiView.as_view(), name='registration' ),
     path('token/login/', CustomObtainAuthToken.as_view(), name='token-login'),
-    path('token/logout/', CustomDiscardAuthToken.as_view(), name='token-logout')
+    path('token/logout/', CustomDiscardAuthToken.as_view(), name='token-logout'),
+
+    path('jwt/create/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('change-password/', ChangePasswordApiView.as_view(), name='change-password'),
+
+    path('profile/', ProfileApiView.as_view(), name='profile'),
 ]
