df179126627521ddb7049f82631bd3900b4356d8
Amirhossein <amirhhdr13832@gmail.com>
2026-07-25 18:50:49 +0330

    Update urls.py

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from accounts.api.v1.views import * 
+#from rest_framework.authtoken.views import ObtainAuthToken
+
 
 
 app_name = 'api-v1'
@@ -7,5 +9,7 @@ app_name = 'api-v1'
 urlpatterns = [
 
     # registration
-    path('registration/',RegistrationApiView.as_view(), name='registration' )
+    path('registration/',RegistrationApiView.as_view(), name='registration' ),
+    path('token/login/', CustomObtainAuthToken.as_view(), name='token-login'),
+    path('token/logout/', CustomDiscardAuthToken.as_view(), name='token-logout')
 ]
