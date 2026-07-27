5316ba3f2876b6fdfe5655dc761a4395efa2e4b1
Amirhossein <amirhhdr13832@gmail.com>
2026-07-28 02:31:50 +0330

    Update accounts.py

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -1,25 +1,42 @@
 from django.urls import path
-from accounts.api.v1.views import * 
-from rest_framework_simplejwt.views import (TokenRefreshView, TokenVerifyView)
-
+from accounts.api.v1.views import *
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
-
     # registration
-    path('registration/',RegistrationApiView.as_view(), name='registration' ),
+    path("registration/", RegistrationApiView.as_view(), name="registration"),
     # login token
-    path('token/login/', CustomObtainAuthToken.as_view(), name='token-login'),
-    path('token/logout/', CustomDiscardAuthToken.as_view(), name='token-logout'),
+    path("token/login/", CustomObtainAuthToken.as_view(), name="token-login"),
+    path(
+        "token/logout/", CustomDiscardAuthToken.as_view(), name="token-logout"
+    ),
     # login JWT
-    path('jwt/create/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path(
+        "jwt/create/",
+        CustomTokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
     # change password
-    path('change-password/', ChangePasswordApiView.as_view(), name='change-password'),
+    path(
+        "change-password/",
+        ChangePasswordApiView.as_view(),
+        name="change-password",
+    ),
     # activation
-    
-    #resend activation
-
-    path('send-email/' ,EmailSend.as_view(), name='send-email')
-    
+    path(
+        "activation/confirm/<str:token>",
+        ActivationApiView.as_view(),
+        name="activation",
+    ),
+    # resend activation
+    path(
+        "activation/resend/",
+        ActivationResendApiView.as_view(),
+        name="activation-resend",
+    ),
+    # send email
+    path("send-email/", EmailSend.as_view(), name="send-email"),
+    # resent password
 ]
