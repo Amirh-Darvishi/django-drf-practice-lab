43911206987fde44d6eea6d7b348022358ace1ff
Amirhossein <amirhhdr13832@gmail.com>
2026-07-28 02:30:20 +0330

    Update pagination.py

--- core/blog/api/v1/pagination.py
+++ core/blog/api/v1/pagination.py
@@ -7,12 +7,14 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 2
 
     def get_paginated_response(self, data):
-        return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
-            },
-            'total_object': self.page.paginator.count,
-            'total_pages': self.page.paginator.num_pages,
-            'results': data
-        })
+        return Response(
+            {
+                "links": {
+                    "next": self.get_next_link(),
+                    "previous": self.get_previous_link(),
+                },
+                "total_object": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "results": data,
+            }
+        )
