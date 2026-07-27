f4f56442cdd95a9a3ce63b3fbddc467b7463d0d2
Amirhossein <amirhhdr13832@gmail.com>
2026-07-28 02:29:51 +0330

    Update urls.py

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -2,15 +2,14 @@ from django.urls import path, include
 from blog.views import *
 from django.views.generic.base import RedirectView
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-     # template
-     path("index1/", indexView, name='FBV-index'),
-     path("index2/", IndexView.as_view(), name='CBV-index'),
-
-     # redirect
-     path(
+    # template
+    path("index1/", indexView, name="FBV-index"),
+    path("index2/", IndexView.as_view(), name="CBV-index"),
+    # redirect
+    path(
         "go-to-django/",
         RedirectView.as_view(url="https://www.djangoproject.com/"),
         name="redirect-to-django",
@@ -20,23 +19,17 @@ urlpatterns = [
         RedirectView.as_view(pattern_name="blog:CBV-index"),
         name="redirect-to-index",
     ),
-    path("go-to-maktab1/", maktab, name='redirect-to-maktab'),
-    path("go-to-maktab2/", Maktab.as_view(), name='redirect-to-maktab'),
-
+    path("go-to-maktab1/", maktab, name="redirect-to-maktab"),
+    path("go-to-maktab2/", Maktab.as_view(), name="redirect-to-maktab"),
     # list
-    path("post/",PostList.as_view(), name="post-list" ),
-
+    path("post/", PostList.as_view(), name="post-list"),
     # detail
-    path("post/<int:pk>",PostDetail.as_view(), name='post-detail' ),
-
+    path("post/<int:pk>", PostDetail.as_view(), name="post-detail"),
     # form
-    path("post/create/", PostCreate.as_view(),name='post-create'),
-    path("contact/form/", ContactCreate.as_view(),name='contact-form'),
-
-    path("post/<int:pk>/edit/",PostUpdate.as_view(), name='post-edit' ),
-
-    path("post/<int:pk>/delete/",PostDelete.as_view(), name='post-delete' ),
-
+    path("post/create/", PostCreate.as_view(), name="post-create"),
+    path("contact/form/", ContactCreate.as_view(), name="contact-form"),
+    path("post/<int:pk>/edit/", PostUpdate.as_view(), name="post-edit"),
+    path("post/<int:pk>/delete/", PostDelete.as_view(), name="post-delete"),
     # api
-    path('api/v1/', include('blog.api.v1.urls'))
+    path("api/v1/", include("blog.api.v1.urls")),
 ]
