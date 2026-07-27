f58f3180e0278339b6d6776627139b761be28f15
Amirhossein <amirhhdr13832@gmail.com>
2026-07-28 02:29:55 +0330

    Update 0003_rename_nmae_category_name.py

--- core/blog/migrations/0003_rename_nmae_category_name.py
+++ core/blog/migrations/0003_rename_nmae_category_name.py
@@ -6,13 +6,13 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0002_alter_post_author'),
+        ("blog", "0002_alter_post_author"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='category',
-            old_name='nmae',
-            new_name='name',
+            model_name="category",
+            old_name="nmae",
+            new_name="name",
         ),
     ]
