6edff0292a7e9bb11675e42fb163a913c8f038ce
Amirhossein <amirhhdr13832@gmail.com>
2026-07-30 14:39:15 +0330

    Update users.py

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -46,9 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     Custom user model for our app
     """
 
-    email = models.EmailField(
-        verbose_name="email address", max_length=255, unique=True
-    )
+    email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
