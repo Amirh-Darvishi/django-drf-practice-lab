df846c64732485091768ee133ccff0d671c5c6f6
Amirhossein <amirhhdr13832@gmail.com>
2026-08-03 13:57:06 +0330

    Update urls.py

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-
+from accounts import views
 app_name = "accounts"
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path("api/v1/", include("accounts.api.v1.urls")),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
+    path('test/', views.test, name='test'),
 ]
