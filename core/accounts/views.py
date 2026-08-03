23b9463cde65023403d30cde60448ab08621339f
Amirhossein <amirhhdr13832@gmail.com>
2026-08-03 14:20:12 +0330

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.cache import cache
 import requests
+from django.views.decorators.cache import cache_page
 # Create your views here.
 
+
+@cache_page(300)
 def test(request):
     response = requests.get('https://f02a0381-2920-49e1-8d7e-7862d3b30f41.mock.pstmn.io/test/delay/5')
     return JsonResponse(response.json())
