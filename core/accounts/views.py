b20d5708a9b1fd3a3104d82d7cdba541238adf76
Amirhossein <amirhhdr13832@gmail.com>
2026-08-03 14:38:26 +0330

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http importh HttpResponse ,JsonResponse
+from django.http import HttpResponse ,JsonResponse
 import requests
 from .tasks import sendEmail
 from time import sleep
