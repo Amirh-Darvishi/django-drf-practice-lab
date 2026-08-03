e2cb10fc914a7236ef989cc237d6215b1ee85175
Amirhossein <amirhhdr13832@gmail.com>
2026-08-03 13:57:08 +0330

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import requests
 # Create your views here.
+
+def test(request):
+    response = requests.get('https://f02a0381-2920-49e1-8d7e-7862d3b30f41.mock.pstmn.io/test/delay/5')
+    return JsonResponse(response.json())
