b9cd06886a8bfc76acabbec24c678824ee1b08ad
Amirhossein <amirhhdr13832@gmail.com>
2026-08-08 17:46:05 +0330

    final test for auto deployment

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 from django.urls import re_path
 from rest_framework import permissions
@@ -39,11 +40,17 @@ schema_view = get_schema_view(
 )
 
 
+
+def indexView(request):
+    return HttpResponse("<h1> Django Final Test </h1")
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blog/", include("blog.urls")),
     path("accounts/", include("accounts.urls")),
     path("api-auth/", include("rest_framework.urls")),
+    path("", indexView, name="index"),
     path(
         "swagger/output.<format>",
         schema_view.without_ui(cache_timeout=0),
